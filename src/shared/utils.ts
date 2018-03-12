161b720c18951da7985cf1d713d281339de0cdf7
bfdes <BFdes@users.noreply.github.com>
2018-03-12 00:36:42 +0000

    Use native functions

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,12 +1,5 @@
 export const parseDate = (timestamp: number) => {
   const date = new Date(1000*timestamp)
-  const monthNames = [
-    'January', 'February', 'March', 'April',
-    'May', 'June', 'July', 'August',
-    'September', 'October', 'November', 'December'
-  ]
-  const day = date.getDate()
-  const month = monthNames[date.getMonth()]
-  const year = date.getFullYear()
-  return `${day} ${month} ${year}`
+  const options = {year: 'numeric', month: 'long', day: 'numeric'}
+  return date.toLocaleDateString('en-gb', options)
 }
