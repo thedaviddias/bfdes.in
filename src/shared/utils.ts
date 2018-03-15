c2517a407eb0cb0df3092086bb010ce553171856
bfdes <BFdes@users.noreply.github.com>
2018-03-15 20:50:19 +0000

    Wrappers for making requests

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -6,3 +6,27 @@ export const parseDate = (timestamp: number) => {
 
 export type Post = {title: string, wordCount: number, body: string, tags: string[], created: number}
 export type Posts = {[s: string]: Post}
+
+export class NetworkError extends Error {
+  status: number
+
+  constructor(status: number, message: string) {
+    super(message)
+    this.status = status
+    Error.captureStackTrace(this, NetworkError)
+  }
+}
+
+const client = (req: Request) => 
+  fetch(req).then(res => 
+    res.json().then(data => {
+      const { status } = res
+      return (status >= 200 && status < 300) ? data : Promise.reject(new NetworkError(status, data.error.message))
+    })
+  )
+
+export const get = (url: string) => 
+  client(new Request(url, {
+    method: 'get',
+    headers: new Headers({'content-type': 'application/json'})
+  }))
