580e0132a377314d138f9fa990d2962eaa329f79
bfdes <bfdes@users.noreply.github.com>
2018-04-01 01:14:10 +0100

    Update to Webpack 4

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -1,6 +1,6 @@
 const [client, server] = require('./base.config');
 
 module.exports = [
-  {...client, ...{devtool: 'inline-source-map'}},
-  {...server, ...{devtool: 'inline-source-map'}}
+  client,
+  server
 ]
--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,6 +1,6 @@
 const [client, server] = require('./base.config');
 
 module.exports = [
-  {...client, ...{devtool: 'source-map'}},
-  {...server, ...{devtool: 'source-map'}}
+  client,
+  server
 ]
