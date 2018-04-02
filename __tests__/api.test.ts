c4454c857a14b5c3f08d6a58b49e28c2a6df45ea
bfdes <bfdes@users.noreply.github.com>
2018-04-02 23:54:57 +0100

    Namespacing

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -20,48 +20,52 @@ const fixture = {
 
 const app = factory(fixture)
 
-test('all posts returned for GET /posts', () => 
-  request(app)
-    .get('/api/posts')
-    .expect(200)
-    .then(res => {
-      expect(res.body.length).toBe(2)
-    })
-)
-
-test('posts filtered by tag correctly', () => 
-  request(app)
-    .get('/api/posts?tag=Algorithms')
-    .expect(200)
-    .then(res => {
-      expect(res.body.length).toBe(1)
-    })
-)
-
-test('post can be fetched by slug', () => {
-  request(app)
-    .get('/api/posts/my-first-post')
-    .expect(200)
-    .then(res => {
-      expect(res.body).toEqual({
-        title: 'My first post',
-        body: 'Lorem ipsum delorum sit amet',
-        created: 1523401200000,
-        tags: ['Algorithms', 'Java'],
-        wordCount: 5
+describe('test GET /posts', () => {
+  test('all posts returned for GET /posts', () => 
+    request(app)
+      .get('/api/posts')
+      .expect(200)
+      .then(res => {
+        expect(res.body.length).toBe(2)
       })
-    })
+  )
+
+  test('posts filtered by tag correctly', () => 
+    request(app)
+      .get('/api/posts?tag=Algorithms')
+      .expect(200)
+      .then(res => {
+        expect(res.body.length).toBe(1)
+      })
+  )
 })
 
-test('404 response returned for non-existent post', () => {
-  request(app)
-    .get('/api/posts/my-third-post')
-    .expect(404)
-    .then(res => {
-      expect(res.body).toEqual({
-        error: {
-          message: "404: No post with that slug"
-        }
+describe('test GET /posts/:slug', () => {
+  test('post can be fetched by slug', () => {
+    request(app)
+      .get('/api/posts/my-first-post')
+      .expect(200)
+      .then(res => {
+        expect(res.body).toEqual({
+          title: 'My first post',
+          body: 'Lorem ipsum delorum sit amet',
+          created: 1523401200000,
+          tags: ['Algorithms', 'Java'],
+          wordCount: 5
+        })
       })
-    })
+  })
+
+  test('404 response returned for non-existent post', () => {
+    request(app)
+      .get('/api/posts/my-third-post')
+      .expect(404)
+      .then(res => {
+        expect(res.body).toEqual({
+          error: {
+            message: "404: No post with that slug"
+          }
+        })
+      })
+  })
 })
