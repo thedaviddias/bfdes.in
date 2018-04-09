49f57de990d221451935a6cbec32662acfe51d61
bfdes <bfdes@users.noreply.github.com>
2018-04-09 08:27:22 +0100

    Tests for Post component; cannot generally have numeric class names

--- tests/component.test/post.test.tsx
+++ tests/component.test/post.test.tsx
@@ -1,12 +1,19 @@
 import * as React from 'react'
-import { Link, MemoryRouter } from 'react-router-dom'
-import { configure, shallow, mount, render } from 'enzyme'
+import { MemoryRouter } from 'react-router-dom'
+import { configure, shallow, mount } from 'enzyme'
 import * as Adapter from 'enzyme-adapter-react-16'
 
-import { PostOr404 } from '../../src/shared/components/'
-import Spinner from '../../src/shared/components/Spinner'
+import { PostOr404 } from '../../src/shared/components'
 
-const mockPost = {
+type Post = {
+  title: string,
+  body: string,
+  wordCount: number,
+  tags: string[],
+  created: number
+}
+
+const fixture = {
   title: 'My first post',
   body: 'Lorem ipsum delorum sit amet',
   wordCount: 5,
@@ -14,11 +21,16 @@ const mockPost = {
   created: 1523401200000
 }
 
+const mockPromise = Promise.resolve(fixture)
+
 jest.mock('../../src/shared/utils', () => ({
   parseDate: require.requireActual('../../src/shared/utils').parseDate,
-  delay: require.requireActual('../../src/shared/utils').delay,
-  get: jest.fn((url: string) => Promise.resolve(mockPost))
+  NetworkError: require.requireActual('../../src/shared/utils').NetworkError,
+  delay: (promise: Promise<any>, interval: number) => promise,
+  get: jest.fn()
 }))
+
+import { get, NetworkError } from '../../src/shared/utils'
 
 beforeAll(() => {
   configure({adapter: new Adapter()})
@@ -30,13 +42,77 @@ describe('<PostOr404 />', () => {
       (global as any).__isBrowser__ = false
     })
 
-    it('displays post on server', () => {
+    it('displays post', () => {
       const wrapper = mount(
         <MemoryRouter>
-          <PostOr404 staticContext={{data: mockPost}}/>
+          <PostOr404 staticContext={{data: fixture}}/>
         </MemoryRouter>
       )
       expect(wrapper.find('.post')).toHaveLength(1)
+    })
+  })
+
+  describe('<PostOr404 /> on client', () => {
+    beforeAll(() => {
+      (global as any).__isBrowser__ = true
+    })
+
+    it('fetches the correct post', () => {
+      (get as jest.Mock<Promise<Post>>).mockReturnValue(mockPromise)
+      
+      const wrapper = mount(
+        <MemoryRouter>
+          <PostOr404 slug='my-first-post'/>
+        </MemoryRouter>
+      )
+      return mockPromise.then(() => {
+        expect(get).toHaveBeenCalledWith('/api/posts/my-first-post')
+      })
+    })
+
+    it('displays post', () => {
+      (get as jest.Mock<Promise<Post>>).mockReturnValue(mockPromise)
+
+      const wrapper = mount(
+        <MemoryRouter>
+          <PostOr404 />
+        </MemoryRouter>
+      )
+      return mockPromise.then(() => {
+        expect(wrapper.update().find('.post')).toHaveLength(1)
+      })
+    })
+
+    it('displays <NoMatch /> when post does not exist', () => {
+      const mockPromise = Promise.reject(new NetworkError(404, "404: No post with that slug"));
+      (get as jest.Mock<Promise<Post>>).mockReturnValue(mockPromise)
+
+      const wrapper = mount(
+        <MemoryRouter>
+          <PostOr404 />
+        </MemoryRouter>
+      )
+      return mockPromise
+        .then(() => {})
+        .catch(() => {
+          expect(wrapper.update().find('.fourOhFour')).toHaveLength(1)
+        })
+    })
+    
+    it('displays error message for failed request', () => {
+      const mockPromise = Promise.reject(new Error);
+      (get as jest.Mock<Promise<Post>>).mockReturnValue(mockPromise)
+
+      const wrapper = mount(
+        <MemoryRouter>
+          <PostOr404 />
+        </MemoryRouter>
+      )
+      return mockPromise
+        .then(() => {})
+        .catch(() => {
+          expect(wrapper.update().find('.error')).toHaveLength(1)
+        })
     })
   })
 })
