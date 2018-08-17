485b0a9451274dad94cda296c0c4d37af760e2ba
bfdes <bfdes@users.noreply.github.com>
2018-08-18 00:42:38 +0100

    Fix typo

--- src/shared/components/About.tsx
+++ src/shared/components/About.tsx
@@ -4,7 +4,7 @@ export default () => (
   <div>
     <h1>About</h1>
 
-    <p>Hello, my name is Bruno Fernandes and work as a software developer.</p>
+    <p>Hello, my name is Bruno Fernandes and I work as a software developer.</p>
     <p>My interests include reactive programming and applied maths (in relation to statistical learning).</p>
     <p>The source code for this website, and other open source work, can be found on <a href="https://www.github.com/bfdes">GitHub</a>.</p>
   </div>
