7d6761d790dc34ddfb769d82db70d591f9e2de57
bfdes <bfdes@users.noreply.github.com>
2018-09-05 21:32:47 +0100

    Improve accessibility

--- src/shared/components/Sidebar.tsx
+++ src/shared/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 export default () => (
   <aside id='sidebar'>
-    <img className='avatar' src={require('../images/avatar.jpg')}></img>
+    <img className='avatar' src={require('../images/avatar.jpg')} alt='Profile photo'></img>
     <div id='nav'>
       <NavLink to='/posts' className='nav-item' activeClassName='active'><h2>Blog</h2></NavLink>
       <NavLink to='/about' className='nav-item' activeClassName='active'><h2>About</h2></NavLink>
