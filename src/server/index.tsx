4139ac30940a6cba91772d1e7af2814c2c94c003
bfdes <bfdes@users.noreply.github.com>
2018-09-05 07:51:17 +0100

    Add Gzip compression

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -2,6 +2,7 @@ import * as path from 'path'
 import * as bodyParser from 'body-parser'
 import * as logger from 'morgan'
 import * as express from 'express'
+import * as compression from 'compression'
 import { Request, Response, NextFunction } from 'express'
 
 import router from './router'
@@ -11,11 +12,20 @@ export default function factory(posts: Posts) {
   const mode = process.env.NODE_ENV
 
   const app = express()
-  // Apply middleware stack
+
+  // Apply middleware stack:
+  // i) logger
   app.use(mode == 'production' ? logger('common') : logger('dev'))
+
+  // ii) request parser
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json())
+
+  // ii) static server w/ gzip compression
+  app.use(compression())
   app.use(express.static(path.resolve('dist', 'static')))
+
+  // iii) router
   app.use('/', router)
 
   // Error handler
