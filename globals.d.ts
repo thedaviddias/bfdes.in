52b1eaedd8ed3626845065fe266cab62af14ad57
Bruno Fernandes <bfdes@users.noreply.github.com>
2019-06-26 21:54:46 +0100

    Use interfaces instead

--- globals.d.ts
+++ globals.d.ts
@@ -1,18 +1,13 @@
-declare type Post = {
+declare interface PostStub {
   title: string,
   slug: string,
   wordCount: number,
-  body: string,
   tags: string[],
   created: number
 }
 
-declare type PostStub = {
-  title: string,
-  slug: string,
-  wordCount: number,
-  tags: string[],
-  created: number
+declare interface Post extends PostStub {
+  body: string
 }
 
 declare const __isBrowser__: boolean  // Injected by Webpack to indicate whether we are running JS on the client or server
