d7eab5b30e92f686809bcf5b845fdfd49142a70c
Bruno Fernandes <bfdes@users.noreply.github.com>
2019-07-04 21:44:55 +0100

    Keep trying

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,9 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   git init
 
   git remote add deploy "ssh://travis@bfdes.in:$SSH_PORT/opt/blog"
+  get config user.name "travis"
+  git config user.password "bfdes@users.noreply.github.com"
+  
   git add .
   git commit -m "Deploy"
   git push --force deploy master
