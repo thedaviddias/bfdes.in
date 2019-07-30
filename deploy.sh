f6f7af41beccbda3f0c44a32b3344cb098bbe60a
Bruno Fernandes <bfdes@users.noreply.github.com>
2019-07-30 07:38:45 +0100

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 yarn build:prod
 
 cd dist
+rm -rf .git
 git init
 git remote add origin git@bfdes.in:~/blog/.git
 git add .
-git commit --allow-empty -m ""
+git commit --allow-empty-message -m ""
 git push origin master --force
