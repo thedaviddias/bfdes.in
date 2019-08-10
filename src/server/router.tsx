6871e523338a55da6cb8085e5ebaa3fe9db69fd6
Bruno Fernandes <bfdes@users.noreply.github.com>
2019-08-10 21:55:20 +0100

    Theme Chrome address bar on mobile

--- src/server/router.tsx
+++ src/server/router.tsx
@@ -11,6 +11,7 @@ const header = (initialData: any) =>
   `
     <meta charset="utf8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#202020">
     <meta name="description" content="Personal blog">
     <meta name="author" content="Bruno Fernandes">
     <title>bfdes.in</title>
