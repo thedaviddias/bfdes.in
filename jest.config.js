e1b2b2d86367090746bcbbc5c3af5f9219c56eaf
Bruno Fernandes <bfdes@users.noreply.github.com>
2020-02-12 23:53:35 +0000

    Support package.json import in tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   collectCoverageFrom: ["src/**/*"],
-  moduleDirectories: ["src", "node_modules"],
-  moduleFileExtensions: ["ts", "tsx", "js"],
+  moduleDirectories: [".", "src", "node_modules"],
+  moduleFileExtensions: ["ts", "tsx", "js", "json"],
   setupFiles: ["<rootDir>/jest/setup.js"],
   transform: {
     "\\.(ts|tsx)$": "ts-jest",
