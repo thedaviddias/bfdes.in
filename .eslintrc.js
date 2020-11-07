e5dec5843f0e8737b934fbe1b9259336bb5ac5bd
Bruno Fernandes <bfdes@users.noreply.github.com>
2020-11-07 20:30:31 +0000

    Not needed?

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
   plugins: ["@typescript-eslint"],
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
     "prettier/@typescript-eslint",
