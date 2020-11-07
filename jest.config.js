425c7b956be5aac76e1bf5c5739bec0e53c6356c
Bruno Fernandes <bfdes@users.noreply.github.com>
2020-11-07 10:04:32 +0000

    Gets rids of extraneous options

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   collectCoverageFrom: ["src/**/*"],
   moduleDirectories: [".", "src", "node_modules"],
-  moduleFileExtensions: ["ts", "tsx", "js", "json"],
+  moduleFileExtensions: ["ts", "tsx", "js"],
   transform: {
     "\\.(ts|tsx)$": "ts-jest",
     "\\.(svg|jpg|png)$": "<rootDir>/jest/assetTransformer.js",
